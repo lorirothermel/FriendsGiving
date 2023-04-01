4ed855316e7463326a170bfd678c7cf729885249
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-01 01:51:41 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Lori Rothermel on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
